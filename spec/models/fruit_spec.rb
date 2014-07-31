46696f6987fd11bebe7df770bb6baec06a90b885
Junichi Ito <jit@sonicgarden.jp>
2014-08-01 05:49:20 +0900

    Add spec for ordering.

--- spec/models/fruit_spec.rb
+++ spec/models/fruit_spec.rb
@@ -5,4 +5,12 @@ describe Fruit do
     fruit = build :fruit, name: 'Banana'
     expect(fruit.name).to eq 'Banana'
   end
+  describe 'ordering' do
+    let!(:apple) { create :fruit, name: 'Apple' }
+    let!(:banana) { create :fruit, name: 'Banana' }
+    let!(:orange) { create :fruit, name: 'Orange' }
+    it 'orders by registration' do
+      expect(Fruit.rank(:row_order)).to eq [apple, banana, orange]
+    end
+  end
 end
